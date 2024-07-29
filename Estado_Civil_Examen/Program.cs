02bb618a41c98b657bc86eb541628207d7067865
ABCD <ABCD@DESKTOP-JF5UEPI>
2024-07-29 08:11:25 -0600

    Agregar archivos de proyecto.

---
+++ Estado_Civil_Examen/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estado_Civil_Examen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char eCivil;
+
+            Console.WriteLine("Ingrese un Caracter (C, S, V, D)");
+            eCivil = char.Parse(Console.ReadLine());
+
+            switch (eCivil)
+            {
+                case 'c':
+                case 'C':
+                    Console.WriteLine("Casado");
+                    break;
+
+                case 'v':
+                case 'V':
+                    Console.WriteLine("Casado");
+                    break;
+
+                case 's':
+                case 'S':
+                    Console.WriteLine("Casado");
+                    break;
+
+                case 'd':
+                case 'D':
+                    Console.WriteLine("Casado");
+                    break;
+
+                default:
+                    Console.WriteLine("No esta permitido");
+                    break;
+
+            }
+            Console.Write("Pulse ENTER para salir"); Console.ReadLine();
+        }
+    }
+}    
